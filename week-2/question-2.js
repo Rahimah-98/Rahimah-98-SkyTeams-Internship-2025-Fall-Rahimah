98c4a7c7fe093dfed11ca21693183b31b91a0eb4
Rahimah Ansari <rahimaansari98@gmail.com>
2025-11-18 21:10:11 +0430

    Solved the second question

---
+++ week-2/question-2.js
@@ -0,0 +1,12 @@
+// Find the largest and smallest of three numbers
+
+var a = 4;
+var b = 2;
+var c = 8;
+
+var largest = Math.max(a, b, c);
+var smallest = Math.min(a, b, c);
+
+console.log(
+  `Between these numbers:(${a}, ${b}, ${c}) the largest is: ${largest} and the smallest is ${smallest}`
+);
