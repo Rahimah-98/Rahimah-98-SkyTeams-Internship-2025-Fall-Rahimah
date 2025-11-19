aa357a4bd4c55f1d637bd358346b883f6af6a072
Rahimah Ansari <rahimaansari98@gmail.com>
2025-11-19 17:53:28 +0430

    Added the question-3

---
+++ week-2/dist/question-3.dev.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// Calculate the area and perimeter of a rectangle
+// _ area = width * length;
+// _ perimeter = (width + length) * 2;
+var width = 6;
+var length = 4;
+var area = width * length;
+var perimeter = (width + length) * 2;
+console.log("Area: ".concat(area, "\nPerimeter: ").concat(perimeter));
+//# sourceMappingURL=question-3.dev.js.map
