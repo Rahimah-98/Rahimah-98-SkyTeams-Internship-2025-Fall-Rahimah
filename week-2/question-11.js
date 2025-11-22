a018c7e50810d4e8fd83569eee613b41a89e5735
Rahimah Ansari <rahimaansari98@gmail.com>
2025-11-22 18:20:21 +0430

    solved the question 11

---
+++ week-2/question-11.js
@@ -0,0 +1,28 @@
+// Simple Calculator using switch-case
+
+function calculator(a, b, operator) {
+  switch (operator) {
+    case '+':
+      return a + b;
+
+    case '-':
+      return a - b;
+
+    case '*':
+      return a * b;
+
+    case '/':
+      return a / b;
+
+    case '%':
+      return a % b;
+
+    default:
+      return 'Invalid operator';
+  }
+}
+
+// Example:
+console.log(calculator(10, 5, '+')); // 15
+console.log(calculator(10, 5, '*')); // 50
+console.log(calculator(10, 5, '/')); // 2
