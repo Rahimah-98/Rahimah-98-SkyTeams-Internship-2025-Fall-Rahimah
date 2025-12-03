be6e5107ab1e71f4c2755c33cf2f24badd737967
Rahimah Ansari <rahimaansari98@gmail.com>
2025-12-03 18:01:52 +0430

    Solved the question 17

---
+++ week-2/dist/question-17.dev.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// Sum of first N natural numbers: Calculate and print the sum using a loop.
+var N = 5;
+var sum = N * (N + 1) / 2;
+console.log(sum);
+//# sourceMappingURL=question-17.dev.js.map
---
+++ week-2/question-17.js
@@ -0,0 +1,7 @@
+// Sum of first N natural numbers: Calculate and print the sum using a loop.
+
+let N = 5;
+
+let sum = (N * (N + 1)) / 2;
+
+console.log(sum);
