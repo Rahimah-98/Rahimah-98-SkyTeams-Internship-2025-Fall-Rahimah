040971fd4c7af52955d558012046f6f189db8fe9
Rahimah Ansari <rahimaansari98@gmail.com>
2025-12-03 17:50:43 +0430

    solved the question 16

---
+++ week-2/dist/question-16.dev.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// Print numbers from 1 to 100:  Use a loop to print num\bers sequentially
+for (var i = 1; i <= 100; i++) {
+  console.log(i);
+}
+//# sourceMappingURL=question-16.dev.js.map
