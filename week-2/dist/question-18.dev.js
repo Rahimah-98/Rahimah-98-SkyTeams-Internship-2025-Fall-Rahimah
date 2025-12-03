7430339c1554c5e19a53c7280a69e6dd9f4a65ac
Rahimah Ansari <rahimaansari98@gmail.com>
2025-12-03 18:17:23 +0430

    solved the question 18

---
+++ week-2/dist/question-18.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// Multiplication table of a number: Print the multiplication table for a given number (up to 10 or N).
+var num = 5;
+
+for (var i = 1; i <= 10; i++) {
+  console.log(num * i);
+}
+//# sourceMappingURL=question-18.dev.js.map
---
+++ week-2/question-18.js
@@ -0,0 +1,7 @@
+// Multiplication table of a number: Print the multiplication table for a given number (up to 10 or N).
+
+const num = 5;
+
+for (let i = 1; i <= 10; i++) {
+  console.log(num * i);
+}
