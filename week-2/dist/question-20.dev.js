7fea3732721abc9ca2e7d5bd4cfae01401988b79
Rahimah Ansari <rahimaansari98@gmail.com>
2025-12-03 18:35:32 +0430

    solved the question 20

---
+++ week-2/dist/question-20.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// Count digits in a number: Find how many digits are in a number.
+var countNumLength = function countNumLength(num) {
+  return num.toString().length;
+};
+
+console.log(countNumLength(12345)); // Output: 5
+//# sourceMappingURL=question-20.dev.js.map
