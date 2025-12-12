fc4ba9270d038dfe1b9434f00ebb4d0acaff3c9a
Rahimah Ansari <rahimaansari98@gmail.com>
2025-12-12 17:53:43 +0430

    solved the question-6

---
+++ week-3/dist/question-6.dev.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// Write a function to find the smallest element in an array:  Find and return the smallest number in an array
+function smallestNum(arr) {
+  var smallest = arr[0];
+
+  for (var i = arr.length - 1; i >= 0; i--) {
+    if (arr[i] < smallest) {
+      smallest = arr[i];
+    }
+  }
+
+  return smallest;
+}
+
+console.log(smallestNum([12, 2, 3, 15, 20, 4, 5])); // Output: 2
+
+console.log(smallestNum([-10, -5, -3, -1])); // Output: -10
+
+console.log(smallestNum([1, 2, 3, 4, 5])); // Output: 1
+//# sourceMappingURL=question-6.dev.js.map
