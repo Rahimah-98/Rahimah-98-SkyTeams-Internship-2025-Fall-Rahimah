d1e01692bc99b4e143d8dbf4b23c7930296e5780
Rahimah Ansari <rahimaansari98@gmail.com>
2025-12-12 18:36:31 +0430

    solved the question-10

---
+++ week-3/question-10.js
@@ -0,0 +1,15 @@
+// Check whether a number is prime:  Determine if a number is divisible only by 1 and itself
+
+function isPrime(num) {
+  if (num >= 1) {
+    for (let i = 2; i < num; i++) {
+      if (num % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+console.log(isPrime(7)); // true
+console.log(isPrime(10)); // false
