1f7123e39da32f1dcda6e123e42315e12507cc9f
Rahimah Ansari <rahimaansari98@gmail.com>
2025-12-14 22:41:16 +0430

    solved question-28

---
+++ week-3/dist/question-28.dev.js
@@ -0,0 +1,21 @@
+"use strict";
+
+//  Remove Duplicates from an Array: Remove duplicate elements efficiently from an array
+// function removeDuplicates(arr) {
+//   return arr.filter((item, index) => arr.indexOf(item) === index);
+// }
+// console.log(removeDuplicates([3, 1, 3, 5, 1])); // Output: [3, 1, 5]
+function removeDuplicates(arr) {
+  var result = [];
+
+  for (var i = 0; i < arr.length; i++) {
+    if (!result.includes(arr[i])) {
+      result.push(arr[i]);
+    }
+  }
+
+  return result;
+}
+
+console.log(removeDuplicates([3, 1, 3, 5, 1])); // Output: [3, 1, 5]
+//# sourceMappingURL=question-28.dev.js.map
