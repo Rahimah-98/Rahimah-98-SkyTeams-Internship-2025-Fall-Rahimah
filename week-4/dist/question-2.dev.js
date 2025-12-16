5ae9eae724d1b9e237ec74d85680a0d8ce229c79
Rahimah Ansari <rahimaansari98@gmail.com>
2025-12-16 22:43:05 +0430

    solved question-2

---
+++ week-4/dist/question-2.dev.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// Merge two sorted arrays into a single sorted array.
+function mergeSortedArrays(arr1, arr2) {
+  return arr1.concat(arr2).sort(function (a, b) {
+    return a - b;
+  });
+}
+
+console.log(mergeSortedArrays([1, 2, 3], [4, 5, 6])); // Output: [1, 2, 3, 4, 5, 6]
+//# sourceMappingURL=question-2.dev.js.map
